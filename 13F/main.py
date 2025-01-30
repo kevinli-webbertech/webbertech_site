c54ad2e6321c86c1c5020938d6df9ada09573ce3
Darren Liu <darrenliu101@gmail.com>
2025-01-30 15:43:06 -0500

    stocks 13f

---
+++ 13F/main.py
@@ -0,0 +1,19 @@
+from filtering_stocks import most_stocks_held, recent_company_stock_filing
+
+recent_company_stock_filing()
+most_stocks_held()
+
+"""
+Beginner Investor
+80% ETFs (e.g., S&P 500 ETF, Total Market ETF, Bond ETF)
+20% Individual Stocks (e.g., Apple, Microsoft, or other well-known companies)
+
+Experienced Investor
+50% ETFs (e.g., Sector ETFs, International ETFs)
+50% Individual Stocks (e.g., Growth stocks, small-cap stocks, or dividend stocks)
+
+Retiree (Conservative)
+90% ETFs (e.g., Bond ETFs, Dividend ETFs, Broad Market ETFs)
+10% Individual Stocks (e.g., Blue-chip companies)
+"""
+
