964961ef47290623937c5be7273eb5c4b3e326a6
Darren Liu <darrenliu101@gmail.com>
2025-02-11 14:14:33 -0500

    13f file that can fetch both private/public companies

--- 13F/edgar_api/13F.py
+++ 13F/edgar_api/13F.py
@@ -1,3 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -5,53 +12,66 @@ from bs4 import BeautifulSoup
 pd.set_option('display.max_rows', None)  
 pd.set_option('display.max_columns', None)  
 pd.set_option('display.width', None) 
-pd.set_option('display.max_colwidth', None) 
+pd.set_option('display.max_colwidth', None)
 
-# required to input email as header to access sec edgar api
 headers = {
     "User-Agent": "darrenliu101@gmail.com"
 }
 
-company_tickers = requests.get("https://www.sec.gov/files/company_tickers.json", headers=headers)
-ciks = company_tickers.json()['0']['cik_str']
+chrome_options = Options()
+chrome_options.add_argument("--headless") 
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36")
 
-company_data = pd.DataFrame.from_dict(company_tickers.json(), orient='index')
-#To see all the tickers and titles
-print(company_data)
+#cik_keys (im gonna put this into mysql db so we can just look up them easier):
+"""https://www.sec.gov/Archives/edgar/cik-lookup-data.txt"""
 
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-#CIK, put in company name to get it, print(company_data) to find your desired company
-input_ticker = "ubs"
-normal_cik = company_data.loc[company_data['ticker'] == input_ticker.upper() , 'cik_str']
-normal_cik = normal_cik.iloc[0] if not normal_cik.empty else None 
-print(f"Ticker found: {normal_cik}")
+cik = '1067983'  #replace with any cik number
 
+driver.get(f"https://www.sec.gov/edgar/browse/?CIK={cik}")
 
-#actual cik has leading zeros so fill
-cik = str(normal_cik).zfill(10)
+# waits for the page to load completely and for the div with data-export
+driver.implicitly_wait(20)  
 
-meta_data = requests.get(f'https://data.sec.gov/submissions/CIK{cik}.json', headers=headers)
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, "html.parser")
 
-#print(meta_data.json()['filings']['recent'].keys())
+# finds the htm links associated with 13f filing (first one is always most recent filed)
+data_export_div = soup.find("div", {"data-export": "Quarterly report filed by institutional managers, Holdings "})
 
-forms = pd.DataFrame.from_dict(meta_data.json()['filings']['recent'])
+if data_export_div:
+    a_tag = data_export_div.find_all("a", class_="filing-link-all-files")
+    if a_tag:
+        for link in a_tag:
+            href = link.get("href")
+            if href and href.endswith(".htm"):
+                print("Found the .htm link:", "https://sec.gov" + href)
+                break
+    else:
+        print("No .htm link found within the div.")
+else:
+    print("Div with the specified data-export attribute not found.")
 
-#print(forms[['accessionNumber','reportDate','form']].head(50))
-
-# find accession number for most recent 13F-HR
-form_13f = forms[forms['form'] == '13F-HR']
-sorted_form_13f = form_13f.sort_values(by='reportDate', ascending=False)
-accession_number = sorted_form_13f.iloc[0]['accessionNumber']
-accession_number_no_dashes = accession_number.replace('-', '')
-
-filing_url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number_no_dashes}/{accession_number}-index.htm"
+filing_url = "https://sec.gov" + href
 print(filing_url)
 
 response = requests.get(filing_url, headers=headers)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
-    
+
+    #Date of the 13F filing
+    date_div = soup.find('div',class_='formGrouping')
+
+    date = date_div.find('div',class_='info').get_text()
+
+    print(date)
+
     links = soup.find_all('a', href=True)
     infotable_links = [link.get('href') for link in links if link.get('href').endswith(('.xml'))]
     
@@ -82,10 +102,12 @@ if response.status_code == 200:
 
         df = pd.DataFrame(data)
         
-        print(df)
+        #prints the dataframe (to showcase stock holdings with total value, and shares owned from each)
+        #print(df)
         
     else:
         print("No xml files found.")
 else:
     print("Failed to get the filing page")
 
+driver.quit()
